5d319e81511a3316efe5130440252d517051ba28
Evaldo Santos <jesantosjr@protonmail.com>
2020-05-29 19:11:46 +0200

    emitting events

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, () => {
